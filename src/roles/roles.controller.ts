d74de2b2721c317d991142ad1bc74e7be3fdb521
theSkyseS <--add>
2023-03-31 20:32:40 +0600

    fix: renamed functions to be more clear fixed api documentation

--- src/roles/roles.controller.ts
+++ src/roles/roles.controller.ts
@@ -1,32 +1,35 @@
-import { Controller, Get, Post, Delete, Param } from '@nestjs/common';
+import { Controller, Get, Post, Param } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { CreateRoleDto } from './dto/create-role.dto';
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { RoleModel } from './roles.model';
 
+@ApiTags('Roles')
 @Controller('roles')
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
-  @ApiOperation({ summary: 'Creates a new user' })
+  @ApiOperation({ summary: 'Creates a new role' })
   @ApiResponse({ status: 201, type: RoleModel })
+  @ApiBody({ type: CreateRoleDto })
   @Post()
-  create(dto: CreateRoleDto) {
-    return this.rolesService.create(dto);
+  create(@Param('roleDto') dto: CreateRoleDto) {
+    return this.rolesService.createRole(dto);
   }
 
-  @ApiOperation({ summary: 'Retrieves all users from the Database' })
+  @ApiOperation({ summary: 'Retrieves all roles from the Database' })
   @ApiResponse({ status: 200, type: [RoleModel] })
   @Get()
   getAll() {
-    return this.rolesService.findAll();
+    return this.rolesService.getAllRoles();
   }
 
-  @ApiOperation({ summary: 'Retrieves all users from the Database' })
+  @ApiOperation({
+    summary: 'Retrieves role with specified name from the Database',
+  })
   @ApiResponse({ status: 200, type: [RoleModel] })
   @Get(':name')
   getByName(@Param('name') name: string) {
-    return this.rolesService.findByName(name);
+    return this.rolesService.getRoleByName(name);
   }
 }
-
