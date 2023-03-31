3b5408b733a0b299e7bce4c84cd466567ca0b164
theSkyseS <--add>
2023-03-31 20:34:15 +0600

    fix: removed id from create-role.dto

--- src/roles/dto/create-role.dto.ts
+++ src/roles/dto/create-role.dto.ts
@@ -1,8 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateRoleDto {
-  @ApiProperty({ description: "Role's ID", example: '2' })
-  readonly id: number;
   @ApiProperty({ description: "Role's Name", example: 'ADMIN' })
   readonly name: string;
 }
