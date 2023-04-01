d4cefd912ada924fce849f37bf3e425fdc9ad7cc
theSkyseS <--add>
2023-04-01 21:04:29 +0600

    misc: export UsersService from UsersModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,14 +1,19 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { UsersController } from './users.controller';
 import { UserModel } from './users.model';
 import { UsersService } from './users.service';
 import { RolesModule } from 'src/roles/roles.module';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService],
-  imports: [SequelizeModule.forFeature([UserModel]), RolesModule],
+  imports: [
+    SequelizeModule.forFeature([UserModel]),
+    RolesModule,
+    forwardRef(() => AuthModule),
+  ],
+  exports: [UsersService],
 })
 export class UsersModule {}
-
