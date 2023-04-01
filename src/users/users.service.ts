7a9fab7c95564c419e49d5d038ed343c7fac39bb
theSkyseS <--add>
2023-04-01 21:03:53 +0600

    feat: added getUserByEmail function fix: fixed role not being assigned to returned object

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,6 +3,7 @@ import { InjectModel } from '@nestjs/sequelize';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserModel } from './users.model';
 import { RolesService } from 'src/roles/roles.service';
+import { RoleModel } from 'src/roles/roles.model';
 
 @Injectable()
 export class UsersService {
@@ -15,10 +16,18 @@ export class UsersService {
     const user = await this.userRepository.create(dto);
     const role = await this.rolesService.getRoleByName('USER');
     await user.$set('roles', [role.id]);
-    return await this.userRepository.create(dto);
+    user.roles = [role];
+    return user;
   }
 
   async getAllUsers(): Promise<UserModel[]> {
-    return await this.userRepository.findAll({ include: { all: true } });
+    return await this.userRepository.findAll({ include: RoleModel });
+  }
+
+  async getUserByEmail(email: string): Promise<UserModel> {
+    return await this.userRepository.findOne({
+      where: { email },
+      include: RoleModel,
+    });
   }
 }
